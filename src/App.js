614ab22bf70c71d24e0682f91fed5ed12a56c1a2
Chandra Kiran <chandrakiran.vish@gmail.com>
2023-12-20 19:17:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to Test 1.
+                    Edit <code>src/App.js</code> and save to Full Test 1.
                 </p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
