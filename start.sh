0164833dfee555dbd3e54881e37ce3412cb370c1
Chandra Kiran <chandrakiran.vish@gmail.com>
2023-12-20 19:16:35 +0530

    first commit

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+pwd=$(pwd)
+osascript -e "tell application \"Terminal\" to do script \"
+cd $pwd
+npm install
+npm start
+\""
+backend="$(pwd)/amplify/backend/function/api/src"
+echo $backend
+osascript -e "tell application \"Terminal\" to do script \"
+cd $backend
+npm install
+npm start 
+\""
